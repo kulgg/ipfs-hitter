f29ce0b175b996be6980c97989fc0bdb2f69c911
kulgg <75735874+kulgg@users.noreply.github.com>
2022-09-18 16:30:14 +0200

    Add inverse_order option

--- ipfs_hitter/modules/ipfs.py
+++ ipfs_hitter/modules/ipfs.py
@@ -10,7 +10,7 @@ class Ipfs:
     def __init__(self):
         self.ipfs_url = "http://ipfs.io/ipfs/"
 
-    async def hit(self, ids_file: str):
+    async def hit(self, ids_file: str, inverse_order: bool = False):
         logging.info("Starting requesting ipfs urls")
         def _hit(url):
             logging.info(url)
@@ -18,14 +18,17 @@ class Ipfs:
                 r = requests.get(url, timeout=500)
                 logging.info(r.status_code)
             except Exception as exception:
-                logging.info("%s: exception", r.url)
+                logging.info("%s: exception", url)
 
 
         ids = open(ids_file, "r")
         urls = [f"{self.ipfs_url}{id}" for id in ids.read().split("\n")]
         ids.close()
 
-        with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+        if inverse_order:
+            urls.reverse()
+
+        with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
             loop = asyncio.get_event_loop()
             tasks = [loop.run_in_executor(executor, _hit, (url)) for url in urls]
 

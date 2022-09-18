5f6060a445be0c340457a4ad53892912e167cc49
kulgg <75735874+kulgg@users.noreply.github.com>
2022-09-18 16:21:01 +0200

    Fix timeouts

--- ipfs_hitter/modules/ipfs.py
+++ ipfs_hitter/modules/ipfs.py
@@ -1,4 +1,5 @@
 
+import asyncio
 import concurrent.futures
 import logging
 
@@ -9,23 +10,26 @@ class Ipfs:
     def __init__(self):
         self.ipfs_url = "http://ipfs.io/ipfs/"
 
-    def hit(self, ids_file: str):
+    async def hit(self, ids_file: str):
         logging.info("Starting requesting ipfs urls")
         def _hit(url):
-            logging.info("Requesting %s", url)
-            r = requests.get(url)
-            return r.status_code
+            logging.info(url)
+            try:
+                r = requests.get(url, timeout=500)
+                logging.info(r.status_code)
+            except Exception as exception:
+                logging.info("%s: exception", r.url)
 
-        with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
-            futures = []
-            with open(ids_file, "r") as ids:
-                for id in ids:
-                    url = f"{self.ipfs_url}{id}"
-                    futures.append(executor.submit(_hit, url))
 
-            for future in concurrent.futures.as_completed(futures):
-                try:
-                    logging.info(future.result())
-                except requests.ConnectTimeout:
-                    logging.exception("Connection timeout")
+        ids = open(ids_file, "r")
+        urls = [f"{self.ipfs_url}{id}" for id in ids.read().split("\n")]
+        ids.close()
+
+        with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+            loop = asyncio.get_event_loop()
+            tasks = [loop.run_in_executor(executor, _hit, (url)) for url in urls]
+
+            
+            for response in await asyncio.gather(*tasks):
+                pass
         logging.info("Finished requesting ipfs urls")
